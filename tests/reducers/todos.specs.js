fea5496fbc2e4f855c6049027e321619ecc1bd6d
Camilo Cortes <Ccortes@hugeinc.com>
2016-08-29 02:29:20 -0500

    [RED] Adding test for TOGGLE_TODO of todos reducer

--- tests/reducers/todos.specs.js
+++ tests/reducers/todos.specs.js
@@ -27,3 +27,42 @@ describe('Add Todo reducer', () => {
     ).toEqual(stateAfter);
   });
 });
+
+const stateBefore2 = [
+  {
+    id: 0,
+    text: 'Learn Redux',
+    completed: false
+  },
+  {
+    id: 1,
+    text: 'Go shopping',
+    completed: false
+  }
+];
+
+const action2 = {
+  type: 'TOGGLE_TODO',
+  id: 1
+};
+
+const stateAfter2 = [
+  {
+    id: 0,
+    text: 'Learn Redux',
+    completed: false
+  },
+  {
+    id: 1,
+    text: 'Go shopping',
+    completed: true
+  }
+];
+
+describe('Toggle To Do reducer', () => {
+  it('Should toggle value of completed property of the given To Do', () => {
+    expect(
+      todos(stateBefore2, action2)
+    ).toEqual(stateAfter2);
+  })
+});
