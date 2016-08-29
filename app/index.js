b57881852e66213e4384dbba37027a49f8f849d1
Camilo Cortes <Ccortes@hugeinc.com>
2016-08-29 03:28:45 -0500

    Showing state tree as an array

--- app/index.js
+++ app/index.js
@@ -1,5 +1,43 @@
-import React from 'react';
-import { render } from 'react-dom';
-import App from './app';
+// import React from 'react';
+// import { render } from 'react-dom';
+// import App from './app';
+import { createStore } from 'redux';
+import todos from './reducers/todos';
 
-render(<App />, document.getElementById('app'));
+const store = createStore(todos);
+
+console.log('Initial state:');
+console.log(store.getState());
+console.log('--------------');
+
+console.log('Dispatching ADD_TODO');
+store.dispatch({
+  type: 'ADD_TODO',
+  id: 0,
+  text: 'Learn Redux'
+});
+console.log('Current state:');
+console.log(store.getState());
+console.log('--------------');
+
+console.log('Dispatching ADD_TODO');
+store.dispatch({
+  type: 'ADD_TODO',
+  id: 1,
+  text: 'Go shopping'
+});
+console.log('Current state:');
+console.log(store.getState());
+console.log('--------------');
+
+console.log('Dispatching TOGGLE_TODO');
+store.dispatch({
+  type: 'TOGGLE_TODO',
+  id: 0
+});
+console.log('Current state:');
+console.log(store.getState());
+console.log('--------------');
+
+
+// render(<App />, document.getElementById('app'));
