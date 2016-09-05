c3cfbce23197cc3d67347b353520b2f395f688a3
Camilo Cortes <Ccortes@hugeinc.com>
2016-09-05 04:25:15 -0500

    [REFACTOR] Generating containers with connect from React-Redux library

--- app/modules/visible-todo-list.js
+++ app/modules/visible-todo-list.js
@@ -1,78 +1,56 @@
-import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
 import { TodoList } from '../components/components';
 
-class VisibleTodoList extends Component {
+const getVisibleTodos = (
+  todos,
+  filter
+) => {
+  let filteredTodos;
 
-  /*
-  static propTypes = {
-    store: PropTypes.object
-  };
-*/
-
-  componentDidMount() {
-    const { store } = this.context;
-    this.unsubscribe = store.subscribe(() =>
-      this.forceUpdate()
-    );
+  switch (filter) {
+    case 'SHOW_ALL':
+      filteredTodos = todos;
+      break;
+    case 'SHOW_COMPLETED':
+      filteredTodos = todos.filter(
+        t => t.completed
+      );
+      break;
+    case 'SHOW_ACTIVE':
+      filteredTodos = todos.filter(
+        t => !t.completed
+      );
+      break;
+    default:
+      filteredTodos = todos;
   }
 
-  componentWillUnmount() {
-    this.unsubscribe();
-  }
-
-  getVisibleTodos = (
-    todos,
-    filter
-  ) => {
-    let filteredTodos;
-
-    switch (filter) {
-      case 'SHOW_ALL':
-        filteredTodos = todos;
-        break;
-      case 'SHOW_COMPLETED':
-        filteredTodos = todos.filter(
-          t => t.completed
-        );
-        break;
-      case 'SHOW_ACTIVE':
-        filteredTodos = todos.filter(
-          t => !t.completed
-        );
-        break;
-      default:
-        filteredTodos = todos;
-    }
-
-    return filteredTodos;
-  };
-
-  render() {
-    const store = this.context.store;
-    const state = store.getState();
-
-    return (
-      <TodoList
-        todos={
-          this.getVisibleTodos(
-            state.todos,
-            state.visibilityFilter
-          )
-        }
-        onTodoClick={id =>
-          store.dispatch({
-            type: 'TOGGLE_TODO',
-            id
-          })
-        }
-      />
-    );
-  }
-}
-
-// We need to tell React which context we want to receive
-VisibleTodoList.contextTypes = {
-  store: PropTypes.object
+  return filteredTodos;
 };
+
+const mapStateToProps = (state) => {
+  return {
+    todos: getVisibleTodos(
+      state.todos,
+      state.visibilityFilter
+    )
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onTodoClick: (id) => {
+      dispatch({
+        type: 'TOGGLE_TODO',
+        id
+      });
+    }
+  };
+};
+
+const VisibleTodoList = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(TodoList);
 
 export default VisibleTodoList;
